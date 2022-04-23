a38363407c0e0ee8f621b2c492db1523813e5cb7
Chukwuma Jonathan <101060691+cjayjonathan@users.noreply.github.com>
2022-04-23 16:28:45 +0100

    Add files via upload

---
+++ Reading Data from Excel for Custom HTML Project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_excel(r'C:\seleniumdrivers\Dat Science Jobs.xlsx')
+print(df)
+
+
+# In[ ]:
+
+
+
+
